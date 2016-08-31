2a5168fd85002f33d5b9806a94cafc515588d53b
Jhuly <jhulyuk@gmail.com>
2016-08-31 19:14:57 -0300

    First Steps

---
+++ app/src/main/java/com/jhuly/wtcs/Activity_Register.java
@@ -0,0 +1,17 @@
+package com.jhuly.wtcs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jhuly on 29/08/2016.
+ */
+public class Activity_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_profi);
+    }
+}

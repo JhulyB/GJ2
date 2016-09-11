8910445f0f8fff22d86d6515eac47f91e2b71c11
Jhuly <jhulyuk@gmail.com>
2016-09-11 14:15:31 -0300

    -ordem de iniciação de activities -lib postgre

---
+++ app/src/main/java/com/jhuly/wtcs/activity_login.java
@@ -0,0 +1,17 @@
+package com.jhuly.wtcs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jhuly on 11/09/2016.
+ */
+public class activity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
